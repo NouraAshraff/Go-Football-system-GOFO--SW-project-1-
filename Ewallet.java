61bf99f9742f66905f4309ed508b85f3b46c4208
Noura Ashraf <nourraasshhraf@gmail.com>
2021-06-12 09:57:36 +0200

    first commit

---
+++ Ewallet.java
@@ -0,0 +1,16 @@
+package GOFO;
+public class Ewallet {
+    private float balance;
+
+    public void setBalance(float balance) {
+        this.balance = balance;
+    }
+
+    public float getBalance() {
+        return balance;
+    }
+
+    public float checkBalance(){
+        return balance;
+    }
+}
